d3415050760e54e33ac8586da9c4a776893076a9
Lakindu Akash <Lakinduakash@gmail.com>
2018-02-24 20:05:49 +0530

    update function generatePassword()

--- index.php
+++ index.php
@@ -84,7 +84,18 @@ function insertUser($app)
 
 function generatePassword()
 {
-
+    $length=7,
+$keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*_-+=`|\(){}[].?'
+) {
+$str = '';
+$max = mb_strlen($keyspace, '8bit') - 1;
+if ($max < 1) {
+throw new Exception('$keyspace must be at least two characters long');
+}
+for ($i = 0; $i < $length; ++$i) {
+$str .= $keyspace[random_int(0, $max)];
+}
+return $str;
 }
 
 
