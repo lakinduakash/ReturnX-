d87063e654b0e371a8deeb45792589555f364a58
Dilina <dilinade@users.noreply.github.com>
2018-02-24 14:27:39 +0530

    Add files via upload
    Basic php RESTful API 
    Run using php -S localhost:8090 /ap

---
+++ api.php
@@ -0,0 +1,16 @@
+<?php
+header("Content-Type:application/json");
+
+response(200,"Success",NULL);
+
+function response($status,$status_message,$data)
+{
+	header("HTTP/1.1 ".$status);
+	
+	$response['status']=$status;
+	$response['status_message']=$status_message;
+	$response['data']=$data;
+	
+	$json_response = json_encode($response);
+	echo $json_response;
+}
